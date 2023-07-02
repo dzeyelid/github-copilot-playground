4830d955aee25e0ccbd878130b348d4e48e93cba
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-07-03 00:18:13 +0900

    Update astro.config.mjs

--- sample/chart/app/astro.config.mjs
+++ sample/chart/app/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dzeyelid.github.io'
+  base: 'github-copilot-playground'
+});
